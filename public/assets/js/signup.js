aca24d7c18c5a49b9192da38e02bb1ca80b5e796
Megan Coppins <meganlcoppins@gmail.com>
2020-06-18 16:09:26 -0700

    added username input to signup form and refactored function to add user

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
+  var usernameInput = $("input#username-input");
   var show = $(".show");
   var hide = $(".hide");
 
@@ -15,23 +16,26 @@ $(document).ready(function() {
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      username: usernameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email || !userData.password || !userData.username) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.username, userData.email, userData.password);
     emailInput.val("");
+    usernameInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(username, email, password) {
     $.post("/api/signup", {
+      username: username,
       email: email,
       password: password
     })
